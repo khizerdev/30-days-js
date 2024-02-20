951ad5689288038adf5097988544493ae6b01eca
khizerdev <46632721+khizerdev@users.noreply.github.com>
2024-02-21 01:09:37 +0500

    Initial commit

---
+++ array-reduce-transformation.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function (nums, fn, init) {
+  let res = init;
+  for (let i = 0; i < nums.length; i++) {
+    res = fn(res, nums[i]);
+  }
+  return res;
+};
+
+console.log(
+  reduce(
+    [1, 2, 3, 4],
+    function sum(accum, curr) {
+      return accum + curr;
+    },
+    0
+  )
+);
