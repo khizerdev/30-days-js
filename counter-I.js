88db15e49bea7029b0484b438502fe80827d1c1e
khizerdev <46632721+khizerdev@users.noreply.github.com>
2024-03-01 23:55:03 +0500

    Counter closure check

---
+++ counter-I.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function (n) {
+  return function () {
+    return n++;
+  };
+};
+
+const counter = createCounter(10);
+console.log(counter());
+console.log(counter());
